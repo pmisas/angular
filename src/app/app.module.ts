c87628415c0a94284e30eb7b93f51971061ee1ab
Paula Misas <pmisas@unal.edu.co>
2023-05-18 16:34:09 -0500

    error reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreModule as NgRxStoreModule, ActionReducerMap } from '@ngrx/store'; 
+import { EffectsModule } from '@ngrx/effects/src';
+
+
 
 import { AppComponent } from './app.component';
 import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
@@ -9,12 +13,31 @@ import { ListaDestinosComponent } from './lista-destinos/lista-destinos.componen
 import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
 import { FormDestinoViajeComponent } from './form-destino-viaje/form-destino-viaje.component';
 import { DestinosApiClient } from './models/destinos-api-client.model';
+import { 
+  DestinosViajesState, 
+  reducerDestinosViajes,
+  initializeDestinosViajesState, 
+  DestinosViajesEffects
+} from './models/destinos-viajes-state.model';
 
 const routes : Routes =[
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: ListaDestinosComponent},
   {path: 'destino', component: DestinoDetalleComponent},
 ]
+
+// Redux init
+export interface AppState {
+  destinos: DestinosViajesState;
+}
+
+const reducers: ActionReducerMap<AppState> = {
+  DestinosViajesState: reducerDestinosViajes
+};
+
+const reducersInitialState = {
+  destinos: initializeDestinosViajesState()
+} 
 
 @NgModule({
   declarations: [
@@ -29,6 +52,8 @@ const routes : Routes =[
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forRoot(routes),
+    NgRxStoreModule.forRoot((reducers) as any, { initialState: reducersInitialState }), 
+    EffectsModule.forRoot([DestinosViajesEffects])
   ],
   providers: [
     DestinosApiClient
--- src/app/lista-destinos/lista-destinos.component.ts
+++ src/app/lista-destinos/lista-destinos.component.ts
@@ -1,6 +1,9 @@
 import {  Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { DestinoViaje } from '../models/destino-viaje.model';
 import { DestinosApiClient } from './../models/destinos-api-client.model';
+import { Store } from '@ngrx/store/src';
+import { AppState } from '../app.module';
+import { ElegidoFavoritoAction, NuevoDestinoAction } from '../models/destinos-viajes-state.model';
 
 @Component({
   selector: 'app-lista-destinos',
@@ -12,14 +15,16 @@ export class ListaDestinosComponent implements OnInit {
   //destinos: DestinoViaje[];
   updates: string[];
 
-  constructor(public destinosApiClient:DestinosApiClient) { 
+  constructor(public destinosApiClient:DestinosApiClient, private store: Store<AppState>) { 
     this.onItemAdded = new EventEmitter();
     this.updates = [];
-    this.destinosApiClient.suscribeOnChange((d: DestinoViaje) => {
-      if (d.nombre != "") {
-        this.updates.push('se ha elegido a ' + d.nombre);
-      }
-    });
+    this.store.select(state => state.destinos.favorito)
+      .subscribe(d => {
+        const fav = d;
+        if (d.nombre != "") {
+          this.updates.push('se ha elegido a ' + d.nombre);
+        }
+      });
   }
   ngOnInit() {
     console.log(this.updates);
@@ -33,6 +38,7 @@ export class ListaDestinosComponent implements OnInit {
   agregado(d: DestinoViaje) {
     this.destinosApiClient.add(d);
     this.onItemAdded.emit(d);
+    this.store.dispatch(new NuevoDestinoAction(d));
   }
 
 
@@ -42,5 +48,6 @@ export class ListaDestinosComponent implements OnInit {
     //se marca el elegido
     //d.setSelected(true);
     this.destinosApiClient.elegir(e);
+    this.store.dispatch(new ElegidoFavoritoAction(e));
   }
 }
